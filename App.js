9ae455864f47caf77636e45696b71f672cc343e0
saattaja <janita.elisa@outlook.com>
2023-09-30 22:04:21 +0300

    listaa etu ja sukunimen

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {DATA} from './Data'
+import Row from './components/Row';
+import  Constants  from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      data={DATA}
+      renderItem={({item})=>(
+        <Row person={item}></Row>)}>
+      </FlatList>
+      <StatusBar style="auto"></StatusBar>
+    </SafeAreaView>
   );
 }
+
+//const renderItem=({item})=>(
+  //<Text>{item.lastname}</Text>)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    marginLeft: 20,
+    marginTop: 20,
+    //alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight: 0,
   },
 });
---
+++ Data.js
@@ -0,0 +1,62 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Kaikkonen',
+        firstname:'Janita'
+    },
+    {
+        id:'2',
+        lastname: 'Riikonen',
+        firstname:'Jesse'
+    },
+    {
+        id:'3',
+        lastname:'Palovaara',
+        firstname:'Veeti'
+    },
+    {
+        id:'4',
+        lastname: 'Pikkarainen',
+        firstname:'Einari'
+    },
+    {
+        id:'5',
+        lastname:'Hämeenaho',
+        firstname:'Eetu'
+    },
+    {
+        id:'6',
+        lastname: 'Tervonen',
+        firstname:'Tuukka'
+    },
+    {
+        id:'7',
+        lastname:'Hyvönen',
+        firstname:'Janne'
+    },
+    {
+        id:'8',
+        lastname: 'Riikonen',
+        firstname:'Miko'
+    },
+    {
+        id:'9',
+        lastname:'Kaikkonen',
+        firstname:'Jarno'
+    },
+    {
+        id:'10',
+        lastname:'Huusko',
+        firstname:'Jussi'
+    },
+    {
+        id:'11',
+        lastname:'Palovaara',
+        firstname:'Leevi'
+    },
+    {
+        id:'12',
+        lastname:'Pillukeisari',
+        firstname:'Konsta'
+    }
+]
